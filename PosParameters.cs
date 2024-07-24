0c55b8d550983af2eb3dfd2269a7e8afc5d1eb79
KENNETH LIM JUN DE 2200817A <2200817A@student.tp.edu.sg>
2024-07-24 12:48:16 +0800

    trtyuio

---
+++ PosParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveyorUtility
+{
+    public class PosParameters
+    {
+        public int POS { get; set; }
+        public int OBJ { get; set; }
+        public int ERR { get; set; }
+        public int SEN1 { get; set; }
+        public int SEN3 { get; set; }
+        public int DROPPED_IN { get; set; }
+        public int MOVED_IN { get; set; }
+        public int OBJ_LEN { get; set; }
+        public int STOP { get; set; }
+    }
+
+}
